bd27cb897bb4f09dbc8800307ccb02f6b5a89bc0
Jonatha Azevedo <johnmasterchip@gmail.com>
2018-10-26 11:05:02 -0300

    Update bcbExpectInf12.R

--- R/bcbExpectInf12.R
+++ R/bcbExpectInf12.R
@@ -12,6 +12,7 @@
 #'
 #'
 #' @import rjson
+#' @importFrom  dplyr bind_rows
 #' @return A data.frame.
 #' @export 
 #'
@@ -30,13 +31,13 @@ bcbExpectInf12 <- function(indicator = 'IPC-FIPE',limit = 100, variables = c("Me
     
     
     indicator = str_replace_all(indicator," ","%20")
-    
+    variables = paste0(variables,collapse = ",")
     if(limit > 10000 | limit < 0)stop("You need provid a limit in between 0 and 10000!")
     # variaveis
     variaveis_a <- paste("filter=Indicador%20eq%20'",indicator,"'",sep="")
-    variaveis_b <- paste("top=",limit,sep="")
-    variaveis_c <- paste("Indicador", "Data","Suavizada",
-                          variables, sep = ",")
+    variaveis_b <- paste("format=json&$top=",limit,sep="")
+    variaveis_c <- paste0("Indicador,", "Data,","Suavizada,",
+                          variables, collapse = ",")
     
     if(missing(start) & missing(end)){
         timespan <- ""
@@ -51,64 +52,10 @@ bcbExpectInf12 <- function(indicator = 'IPC-FIPE',limit = 100, variables = c("Me
     
     baseurl <- "https://olinda.bcb.gov.br/olinda/servico/Expectativas/versao/v1/odata/"
     query_url <- paste(baseurl, "ExpectativasMercadoInflacao12Meses", "?$",variaveis_b,"&$",variaveis_a,timespan,
-                       "&$select=",variaveis_c, sep = "", collapse = "")
+                       "&$select=",paste0(variaveis_c), sep = "", collapse = "")
     
     
-    data <- fromJSON(file = query_url)$value
-    data <- do.call("rbind", lapply(data, as.data.frame))
+    data = bind_rows(fromJSON(file =  query_url)$value)
     
     return(data)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
